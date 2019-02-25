0a282df1a5ec60651f2ba2c2fef6bd7b42d6224e
mao <mao@sitecore.net>
2019-02-25 17:35:02 +0200

    Add default implementation

---
+++ src/Sitecore.Support.192873/ContentSearch/SolrProvider/SolrIndexSchema.cs
@@ -0,0 +1,33 @@
+﻿using SolrNet.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+  public class SolrIndexSchema : Sitecore.ContentSearch.SolrProvider.SolrIndexSchema
+  {
+    private readonly SolrSchema schema;
+
+    private readonly List<string> allFields;
+
+    private readonly List<string> allCultures;
+
+    public override ICollection<string> AllFieldNames => allFields;
+
+    public override ICollection<string> AllCultures => allCultures;
+
+    public new SolrSchema SolrSchema => schema;
+
+    public SolrIndexSchema(SolrSchema schema) : base(schema)
+    {
+      this.schema = schema;
+      allFields = (from x in this.schema.SolrFields
+                   select x.Name).ToList();
+      allCultures = (from x in this.schema.SolrDynamicFields
+                     where x.Name.StartsWith("*_t_")
+                     select x.Name.Replace("*_t", string.Empty)).ToList();
+    }
+  }
+}
---
+++ src/Sitecore.Support.192873/ContentSearch/SolrProvider/SolrSearchIndex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Maintenance;
+using Sitecore.ContentSearch.SolrProvider;
+using SolrNet;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+  public class SolrSearchIndex : Sitecore.ContentSearch.SolrProvider.SolrSearchIndex
+  {
+    private SolrIndexSchema schema;
+
+    public override ISearchIndexSchema Schema => this.Schema;
+
+    public SolrSearchIndex(string name, string core, IIndexPropertyStore propertyStore, string group) : base(name, core, propertyStore, group)
+    {
+      var solrOperations = typeof(Sitecore.ContentSearch.SolrProvider.SolrSearchIndex)
+        .GetProperty("SolrOperations", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(this)
+        as ISolrOperations<Dictionary<string, object>>;
+
+      this.schema = new SolrIndexSchema(solrOperations.GetSchema());
+    }
+  }
+}
